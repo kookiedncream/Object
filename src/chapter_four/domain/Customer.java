0a104b8f635f95fe144808eca54c5dc9bf7f93d3
kookiedncream <kim76744@naver.com>
2022-07-03 20:14:35 +0900

    feat: chapter four
    데이터 중심 설계

---
+++ src/chapter_four/domain/Customer.java
@@ -0,0 +1,11 @@
+package chapter_four.domain;
+
+public class Customer {
+	private String name;
+	private String id;
+
+	public Customer(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+}
---
+++ src/chapter_four/domain/condition/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package chapter_four.domain.condition;
+
+public enum DiscountConditionType {
+	SEQUENCE,
+	PERIOD
+}
